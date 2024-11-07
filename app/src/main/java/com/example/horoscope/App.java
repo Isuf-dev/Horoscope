51ab48001a970742e055b42a6810bd7d30db940e
Mage <muca.isuf06@gmail.com>
2024-11-07 14:20:41 +0100

    Build the startup UI

--- app/src/main/java/com/example/horoscope/App.java
+++ app/src/main/java/com/example/horoscope/App.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class App extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        Optional<Integer> result = Stream.of(1, 2, 3,6,7,8).map(e -> e * 10).reduce((a, b) -> a + b);
+        Integer sum = result.get();
+        System.out.println(sum);
 
     }
 }
